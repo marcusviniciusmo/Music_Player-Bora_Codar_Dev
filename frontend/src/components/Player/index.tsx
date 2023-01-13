9da1a637ef50d6b389b73781b02aeaed5433be25
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2023-01-13 16:03:23 +0000

    PlayerFull component creation

--- frontend/src/components/Player/index.tsx
+++ frontend/src/components/Player/index.tsx
@@ -1,7 +1,11 @@
+import { PlayerFull } from "../PlayerFull";
+
 export function Player() {
   return (
     <>
       <h1>PLAYER Component</h1>
+
+      <PlayerFull/>
     </>
   );
 };
---
+++ frontend/src/components/PlayerFull/index.tsx
@@ -0,0 +1,7 @@
+export function PlayerFull() {
+  return (
+    <>
+      <h1>PLAYER FULL Component</h1>
+    </>
+  );
+};
